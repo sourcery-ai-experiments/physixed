0276d216820c266787ca86672ed5981c84ac3cb9
R4vante <l.teegelbeckers@hotmail.com>
2024-04-26 00:12:52 +0200

    feat(Free fall): :sparkles: add model for free fall
    Add model to simulate an object in free fall

--- px_kinematics/pytools/utils.py
+++ px_kinematics/pytools/utils.py
@@ -1,10 +1,19 @@
 import numpy as np
+import pint
 
-
-from px_main.pytools.utils import ureg, value_parse_unit
+from px_main.pytools.utils import value_parse_unit
 
 
 class FreeFallModel:
+    """Physical model to simulate free fall.
+
+    Attributes
+        initial_h (tuple): the initial height of the object with unit.
+        initial_v (tuple): the initial velocity of the object with unit.
+        time_discretization_step (tuple): time step with unit.
+
+    """
+
     def __init__(
         self,
         initial_h: tuple,
@@ -14,24 +23,27 @@ class FreeFallModel:
         self.initial_h = value_parse_unit(initial_h)
         self.initial_v = value_parse_unit(initial_v)
         self.time_discretization_step = value_parse_unit(time_discretization_step)
+        self.g = value_parse_unit((9.81, "m/s**2"))
 
-    def solve_model(self):
-        g = value_parse_unit((9.81, "m/s**2"))
-        results = {}
-        time_discretization_step = self.time_discretization_step.m_as("s")
-        initial_h = self.initial_h.m_as("m")
-        initial_v = self.initial_v.m_as("m/s")
+    def solve_endtime(self) -> pint.Quantity:
+        """Calculate time till object hits the ground.
 
-        time = np.array([0])
-        height = np.array([initial_h])
-        velocity = np.array([initial_v])
+        Returns
+            pint.Quantity: time till the object hits the ground.
 
-        for _i in range(100):
-            time = np.append(time, time[-1] + time_discretization_step)
-            height = np.append(height, height[-1] - velocity[-1] * time_discretization_step)
-            velocity = np.append(velocity, velocity[-1] + g * time_discretization_step)
+        """
+        return -self.initial_v / self.g + np.sqrt(self.initial_v**2 / self.g**2 + 2 * self.initial_h / self.g)
 
-        results["time"] = time * ureg.Quantity(1, "s")
-        results["height"] = time * ureg.Quantity(1, "m")
-        results["velocity"] = time * ureg.Quantity(1, "m/s")
-        return results
+    def solve_eq(self) -> dict:
+        """Calculate the height and velocity for every time step.
+
+        Returns
+            dict: resulting values for height, velocity and time for every time step.
+
+        """
+        end_time = self.solve_endtime()
+        time = value_parse_unit((np.arange(0, end_time.magnitude, self.time_discretization_step.magnitude), "s"))
+        height = self.initial_h - self.initial_v * time - 0.5 * self.g * time**2
+        velocity = self.initial_v + self.g * time
+
+        return {"height": height, "velocity": velocity, "time": time}
