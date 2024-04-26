422c3646d9838984f73c0bf92fa86dcddeda5b1a
R4vante <l.teegelbeckers@hotmail.com>
2024-04-26 23:26:25 +0200

    chore: include time calculation in free fall view

--- px_kinematics/views.py
+++ px_kinematics/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.template.response import TemplateResponse
 
+from px_main.pytools.utils import ureg
+
 from .forms import FreeFallForm
+from .pytools.utils import FreeFallModel
+
+ureg.default_format = "~P"
 
 
 def ui_landing_page(request: HttpRequest) -> HttpResponse:
@@ -15,14 +20,18 @@ def ui_landing_page(request: HttpRequest) -> HttpResponse:
 
     """
     form = FreeFallForm()
+    time = 0
 
     if request.method == "POST":
         form = FreeFallForm(request.POST)
         if form.is_valid():
             distance = form.cleaned_data["distance"]
+            velocity = form.cleaned_data["velocity"]
+            free_fall_model = FreeFallModel(initial_h=(distance, "m"), initial_v=(velocity, "m/s"))
+            time = free_fall_model.solve_endtime()
     context = {
         "form": form,
-        "distance": distance,
+        "time": time,
     }
 
     return TemplateResponse(request, "px_kinematics/main.html", context)
