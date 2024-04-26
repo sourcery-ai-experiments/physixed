a075e5b6ea6d9b575de4a73700e32d001c5aaf2e
R4vante <l.teegelbeckers@hotmail.com>
2024-04-26 20:56:55 +0200

    test(kinematics): create happy tests for free fall model

--- px_kinematics/tests/test_tools.py
+++ px_kinematics/tests/test_tools.py
@@ -1,19 +1,41 @@
 import pytest
+from pint import Quantity
+
+from ..pytools.utils import FreeFallModel
 
 
 @pytest.fixture()
-def freefall_factory():
-    return FreeFall()
+def free_fall_model() -> FreeFallModel:
+    """Create the free fall instance.
 
-@pytest.mark.parametrize(
-    ("h", "v", "t", "expected"),
-    [
-        (None, 0, 4, 78),
-        (78, None, 4, 39),
-        (None, 0, 4),
-    ]
-)
-def test_freefall(freefall_factory, h, v, t, expected):
-    result = free_fall_factory.calc_params(h,v,t)
-    assert result == expected
+    Returns
+       FreeFallModel: A free fall model instance
 
+    """
+    return FreeFallModel((10, "m"), (0, "m/s"))
+
+
+class TestFreeFallHappy:
+    """Happy path testing for free fall model."""
+
+    def test_solve_endtime(self, free_fall_model: FreeFallModel) -> None:
+        """Test end time method of free fall object.
+
+        Args:
+            free_fall_model (FreeFallModel): Fixture to create the free fall object
+
+        """
+
+        assert isinstance(free_fall_model.solve_endtime(), Quantity)
+        assert free_fall_model.solve_endtime().magnitude == pytest.approx(1.43, rel=0.1)
+        assert free_fall_model.solve_endtime().units == "second"
+
+    def test_solve(self, free_fall_model: FreeFallModel) -> None:
+        """Test model solve of free fall object.
+
+        Args:
+            free_fall_model: Fixture to create the free fall object
+
+        """
+
+        assert isinstance(free_fall_model.solve_eq(), dict)
