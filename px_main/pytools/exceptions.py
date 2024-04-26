d09171823ba3dc1ba126f06c0718cb7d9b0979f8
R4vante <l.teegelbeckers@hotmail.com>
2024-04-26 21:24:27 +0200

    style(exceptions): rename negative number exception class

--- px_main/pytools/exceptions.py
+++ px_main/pytools/exceptions.py
@@ -1,4 +1,4 @@
-class NegativeNumberError(Exception):
+class NegativeValueError(Exception):
     """Raise error if number is smaller than 0."""
 
     def __init__(self, message: str | None = "Number cannot be smaller than 0"):
