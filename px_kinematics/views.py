3bef86bb61aa7e1be3d692f996b4b951656aa265
R4vante <l.teegelbeckers@hotmail.com>
2024-05-04 00:00:00 +0200

    feat(Free fall): Add functionality to generate plot using JS AJAX

--- px_kinematics/views.py
+++ px_kinematics/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.template.response import TemplateResponse
 
 from px_main.pytools.utils import ureg
@@ -26,16 +26,22 @@ def ui_landing_page(request: HttpRequest) -> HttpResponse:
             distance = form.cleaned_data["distance"]
             velocity = form.cleaned_data["velocity"]
             free_fall_model = FreeFallModel(initial_h=(distance, "m"), initial_v=(velocity, "m/s"))
-            time = free_fall_model.solve_endtime()
             results = free_fall_model.solve_eq()
+            out = {
+                "height": [h.magnitude for h in results["height"]],
+                "time": [t.magnitude for t in results["time"]],
+            }
 
+            # Genereer de plot
             fig = make_plot(x_data=results["time"], y_data=results["height"])
-            context = {
-                "form": form,
-                "time": time,
+
+            # Stuur alleen de plotgegevens naar de frontend
+            plot_data = {
+                "x_data": out["time"],
+                "y_data": out["height"],
                 "fig": fig,
             }
-            return TemplateResponse(request, "px_kinematics/main.html", context)
+            return JsonResponse(plot_data)
 
     form = FreeFallForm()
     context = {"form": form}
