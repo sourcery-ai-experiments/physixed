6d109472ea9cf22bd0d256f305d3b32a50fc8dfb
R4vante <l.teegelbeckers@hotmail.com>
2024-05-12 21:39:33 +0200

    refactor: Refactor plot function to handle pint.Quantity

--- px_kinematics/tests/test_lineplot.py
+++ px_kinematics/tests/test_lineplot.py
@@ -1,8 +1,15 @@
 import numpy as np
+import pint
 import pytest
 from numpy.typing import NDArray
 
 from ..pytools.lineplot import make_plot
+
+
+@pytest.fixture()
+def ureg() -> pint.UnitRegistry:
+    """Create unit registry for testing."""
+    return pint.UnitRegistry()
 
 
 @pytest.mark.parametrize(
@@ -14,16 +21,17 @@ from ..pytools.lineplot import make_plot
         (np.linspace(0, 10, 100), np.sin(np.linspace(0, 10, 100))),
     ],
 )
-def test_make_plot_happy(x_data: NDArray | list, y_data: NDArray | list) -> None:
+def test_make_plot_happy(x_data: NDArray | list, y_data: NDArray | list, ureg: pytest.FixtureRequest) -> None:
     """
     Test the make_plot function with happy path inputs.
 
     Args:
-        x_data: The x-axis data for the plot.
-        y_data: The y-axis data for the plot.
+        x_data (NDArray | list): The x-axis data for the plot.
+        y_data (NDArray | list): The y-axis data for the plot.
+        ureg (FixtureRequest): The unit registry for testing.
 
     """
-    result = make_plot(x_data, y_data)
+    result = make_plot(x_data * ureg.m, y_data * ureg.kg)
 
     assert "<html>" in result
     assert "Plotly" in result
@@ -39,6 +47,7 @@ def test_make_plot_sad(x_data: str | list | None, y_data: str | list | None) -> 
     Args:
         x_data (str | list | None): The x-axis data for the plot.
         y_data (str | list | None): The y-axis data for the plot.
+        ureg (FixtureRequest): The unit registry for testing.
 
     Raises:
         TypeError: If the make_plot function does not raise a TypeError when given invalid input types.
