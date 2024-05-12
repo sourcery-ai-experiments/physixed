30c5fa52b7a31b91de88965c2231986ef652838a
R4vante <l.teegelbeckers@hotmail.com>
2024-05-12 02:15:07 +0200

    chore: Add fade-in animation for first plot in free_fall.js

--- px_kinematics/static/px_kinematics/free_fall.js
+++ px_kinematics/static/px_kinematics/free_fall.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  var isFirstPlot = true; // Vlag om bij te houden of dit de eerste keer is dat de plot wordt toegevoegd
+
   $(".input-form").submit(function (event) {
     console.log("Formulier is ingediend!");
     event.preventDefault(); // Voorkom standaardgedrag van formulierinzending
@@ -12,8 +14,14 @@ $(document).ready(function () {
       url: "", // Gebruik huidige URL
       data: formData,
       success: function (data) {
-        // Voeg de plot toe aan de container
-        $(".fig-section").html(data.fig);
+        // Voeg de plot toe aan de container met fade-in animatie alleen bij de eerste keer
+        let plotContainer = $(".fig-section");
+        if (isFirstPlot) {
+          plotContainer.hide().html(data.fig).fadeIn(1000); // 1000 is de duur van de fade-in animatie in milliseconden
+          isFirstPlot = false; // Markeer dat de plot al is toegevoegd
+        } else {
+          plotContainer.html(data.fig); // Voeg de plot toe zonder fade-in animatie
+        }
       },
       error: function (xhr, errmsg, err) {
         console.log(xhr.status + ": " + xhr.responseText); // Log eventuele fouten
